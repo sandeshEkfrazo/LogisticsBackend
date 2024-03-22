fa7d4c35e784a12e7fdb10e47bc7ddad87d7ddc0
sandesh <sandeshdurgekar36@gmail.com>
2024-03-22 12:11:16 +0530

    removed status 9

--- userModule/views.py
+++ userModule/views.py
@@ -255,8 +255,6 @@ class BookVehicleAPI(APIView):
                         # print("finalArr=========>>>>--->>", finalArr)
                         # print("Hi==>>",BookingDetail.objects.filter((Q(driver_id=i['user_id']) & (Q(status_id=1) | Q(status_id=2) | Q(status_id=6) | Q(status_id=8) | Q(status_id=10))) & Q(is_scheduled=True)).values('driver__first_name'))
 
-                        print("sss==>>",i['user_id'])
-
                         # if i['user_id'] is not None:
                         if ScheduledOrder.objects.filter(booking__driver_id=i['user_id']).exists():
                             s = ScheduledOrder.objects.filter(booking__driver_id=i['user_id']).values('scheduled_date_and_time').last()['scheduled_date_and_time']
