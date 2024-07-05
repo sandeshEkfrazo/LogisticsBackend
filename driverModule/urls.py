ce49c289bb0f8a98495ed2e45cbe1a8a8512656a
SheetalKuppast <sheetalkuppast@ekfrazo.in>
2024-07-05 17:02:55 +0530

    updated views urls

--- driverModule/urls.py
+++ driverModule/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+
     path('driver-order-detail', DriverAPI.as_view()),
     path('drivers-order-detail', DriverOrderAPI.as_view()),
     path('update-driver-online-status/', UpdateDriveOnlineApi.as_view()),
@@ -10,5 +11,7 @@ urlpatterns = [
     path('driver-overall-ratings/<int:pk>', DriverEarningsAndratingAPI.as_view()),
     path("notify-driver-document-expiry", NotifyDriverDocumentExpiry.as_view()),
     path("drivertotal_earning/", DriverEarningReport.as_view()),
-    path('assign-vehicle-to-new-driver', AssignVehicleToDriver.as_view())
+    path('assign-vehicle-to-new-driver', AssignVehicleToDriver.as_view()),
+    path('drivers-ride-history', DriverRideHistoryAPI.as_view()),
+    path('calculate-eta/', CalculateETA.as_view(), name='calculate_eta'),
 ]
